653aa9c4f9a7abfc3142f9f77384603dcf2e48f5
Eli Guy <eliguy343@gmail.com>
2023-01-06 15:03:18 +0200

    API - get most active commenter and poster

--- app.py
+++ app.py
@@ -16,13 +16,35 @@ def max_posts_user():
     doc_ref = db.collection(u'posts')
     docs = doc_ref.stream()
     posts = []
+    comments = []
     for doc in docs:
+        comments_ref = db.collection(u'posts').document(doc.id).collection('comments')
+
+        for comment in comments_ref.stream():
+            comments.append(comment.to_dict())
+
         posts.append(doc.to_dict())
+
     posts_df = pd.DataFrame(posts)
     posts_df_grouped = posts_df.groupby("username")["text"].count()
     max_user = posts_df_grouped.idxmax()
     max_amount = posts_df_grouped.max()
-    result = {"user": max_user, "max_posts": max_amount }
+    
+    comments_df = pd.DataFrame(comments)
+    comments_df_grouped = comments_df.groupby("username")["comment"].count()
+    max_user_comments = comments_df_grouped.idxmax()
+    max_amount_comments = comments_df_grouped.max()
+
+    result ={
+        "max_posts":{
+            "user": max_user,
+            "max_posts": max_amount
+        },
+        "max_comments":{
+            "user": max_user_comments,
+            "max_comments": max_amount_comments
+        }
+    }
     result_json = json.dumps(result)
     return result_json
 
