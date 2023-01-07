6a3f1b645ad255418a10f704def79c3b01d0956d
Eli Guy <eliguy343@gmail.com>
2023-01-07 10:48:12 +0200

    API - get top 5 trending

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import pandas as pd
 import json
+import requests
+import nltk
+import string
+import re
+import os
+from collections import Counter
 
 flask_app = Flask(__name__)
+stop_words = ["a", "an","I","i", "the", "this", "that", "it", "is", "really", "to", "and", "for", "these", "those"] + list(string.punctuation)
+
 
 cred = credentials.Certificate("semaphore-a7fd1-firebase-adminsdk-dyeb3-e430d9430a.json")
 app = firebase_admin.initialize_app(cred)
@@ -47,6 +55,32 @@ def max_posts_user():
     }
     result_json = json.dumps(result)
     return result_json
+
+
+@flask_app.route('/trending/top5')
+def trending_words():
+    doc_ref = db.collection(u'posts')
+    docs = doc_ref.stream()
+    posts = []
+    comments = []
+    for doc in docs:
+        posts.append(doc.to_dict())
+
+    posts_df = pd.DataFrame(posts)
+    posts = posts_df['text']
+    filtered_wordlists = []
+
+    for post in posts:
+        wordlist = [word for word in nltk.word_tokenize(post) if word not in stop_words]
+        filtered_wordlists.append(wordlist)
+
+    all_words = [item for sublist in filtered_wordlists for item in sublist]
+    word_counter = Counter(all_words)
+    most_common_words = word_counter.most_common(5)
+    result_json = json.dumps(most_common_words)
+
+    return result_json
+    
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
